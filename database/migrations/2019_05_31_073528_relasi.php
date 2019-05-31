00b96d95fc76c89d424bfc7a1e24164bc1a8f325
bagusyanuar <bagus.yanuar613@gmail.com>
2019-05-31 15:16:39 +0700

    migration add

--- database/migrations/2019_05_31_073528_relasi.php
+++ database/migrations/2019_05_31_073528_relasi.php
@@ -14,6 +14,19 @@ class Relasi extends Migration
     public function up()
     {
         //
+        Schema::table('tb_product', function(Blueprint $table){
+            $table->foreign('kdSatuan','kdsatuanproduk_ifk')->references('kdSatuan')->on('tb_satuan')->onDelete('CASCADE')->onUpdate('CASCADE');
+        });
+
+        Schema::table('tb_barangmasuk', function(Blueprint $table){
+            $table->foreign('kdProduct','kdprodukmasuk_ifk')->references( 'kdProduct')->on('tb_product')->onDelete('CASCADE')->onUpdate('CASCADE');
+        });
+        Schema::table('tb_barangkeluar', function(Blueprint $table){
+            $table->foreign('kdProduct','kdprodukkeluar_ifk')->references( 'kdProduct')->on('tb_product')->onDelete('CASCADE')->onUpdate('CASCADE');
+        });
+        Schema::table( 'tb_penjualan', function(Blueprint $table){
+            $table->foreign( 'customer','customerpenjualan_ifk')->references( 'username')->on('tb_user')->onDelete('CASCADE')->onUpdate('CASCADE');
+        });
     }
 
     /**
@@ -24,5 +37,17 @@ class Relasi extends Migration
     public function down()
     {
         //
+        Schema::table( 'tb_product', function (Blueprint $table) {
+            $table->dropForeign( 'kdsatuanproduk_ifk');
+        });
+        Schema::table( 'tb_barangmasuk', function (Blueprint $table) {
+            $table->dropForeign( 'kdprodukmasuk_ifk');
+        });
+        Schema::table( 'tb_barangkeluar', function (Blueprint $table) {
+            $table->dropForeign( 'kdprodukkeluar_ifk');
+        });
+        Schema::table( 'tb_penjualan', function (Blueprint $table) {
+            $table->dropForeign( 'customerpenjualan_ifk');
+        });
     }
 }
