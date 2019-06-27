14d0c10ce7dc793de091d20cfcf63f3da75e54a7
bagusyanuar <bagus.yanuar613@gmail.com>
2019-06-27 15:58:18 +0700

    migration

--- database/migrations/2019_05_31_060418_tb_default.php
+++ database/migrations/2019_05_31_060418_tb_default.php
@@ -19,48 +19,7 @@ class TbDefault extends Migration
             $table->string('namaSatuan','255');
         });
 
-        Schema::create('tb_product', function(Blueprint $table){
-            $table->string('kdProduct','15')->primary();
-            $table->string('namaProduk','255');
-            $table->string('kdSatuan','10')->index();
-            $table->enum('kategori',['satuan','paket']);
-            $table->integer('hargaJual')->default('0');
-            $table->integer('diskon')->default('0');
-            $table->text('isi');
-            $table->text('keterangan');
-            $table->timestamps();
-        });
-
-        Schema::create('tb_barangmasuk',function(Blueprint $table){
-            $table->bigIncrements('id');
-            $table->string('noTrans','15');
-            $table->string('kdProduct','15')->index();
-            $table->integer('qty')->default('0');
-            $table->date('tglMasuk');
-            $table->text('keterangan');
-            $table->timestamps();
-        });
-
-        Schema::create('tb_barangkeluar', function(Blueprint $table){
-            $table->bigIncrements('id');
-            $table->string('jenisKeluar','3')->default('PNJ');
-            $table->string('noTrans','15');
-            $table->date('tglKeluar');
-            $table->string('kdProduct', '15')->index();
-            $table->integer('qty')->default('0');
-            $table->integer('diskon')->default('0');
-            $table->text('keterangan');
-            $table->enum('status',['0','1']);
-            $table->timestamps();
-        });
-
-        Schema::create('tb_penjualan', function(Blueprint $table){
-            $table->string('noTrans','15')->primary();
-            $table->string('customer','25')->index();
-            $table->date('tglJual');
-            $table->integer('diskon')->default('0');
-        });
-
+       
     }
 
     /**
@@ -72,9 +31,5 @@ class TbDefault extends Migration
     {
         //
         Schema::dropIfExists( 'tb_satuan');
-        Schema::dropIfExists( 'tb_product');
-        Schema::dropIfExists( 'tb_barangmasuk');
-        Schema::dropIfExists( 'tb_barangkeluar');
-        Schema::dropIfExists( 'tb_penjualan');
     }
 }
--- database/migrations/2019_05_31_073528_relasi.php
+++ database/migrations/2019_05_31_073528_relasi.php
@@ -18,15 +18,7 @@ class Relasi extends Migration
             $table->foreign('kdSatuan','kdsatuanproduk_ifk')->references('kdSatuan')->on('tb_satuan')->onDelete('CASCADE')->onUpdate('CASCADE');
         });
 
-        Schema::table('tb_barangmasuk', function(Blueprint $table){
-            $table->foreign('kdProduct','kdprodukmasuk_ifk')->references( 'kdProduct')->on('tb_product')->onDelete('CASCADE')->onUpdate('CASCADE');
-        });
-        Schema::table('tb_barangkeluar', function(Blueprint $table){
-            $table->foreign('kdProduct','kdprodukkeluar_ifk')->references( 'kdProduct')->on('tb_product')->onDelete('CASCADE')->onUpdate('CASCADE');
-        });
-        Schema::table( 'tb_penjualan', function(Blueprint $table){
-            $table->foreign( 'customer','customerpenjualan_ifk')->references( 'username')->on('tb_user')->onDelete('CASCADE')->onUpdate('CASCADE');
-        });
+        
     }
 
     /**
@@ -40,14 +32,6 @@ class Relasi extends Migration
         Schema::table( 'tb_product', function (Blueprint $table) {
             $table->dropForeign( 'kdsatuanproduk_ifk');
         });
-        Schema::table( 'tb_barangmasuk', function (Blueprint $table) {
-            $table->dropForeign( 'kdprodukmasuk_ifk');
-        });
-        Schema::table( 'tb_barangkeluar', function (Blueprint $table) {
-            $table->dropForeign( 'kdprodukkeluar_ifk');
-        });
-        Schema::table( 'tb_penjualan', function (Blueprint $table) {
-            $table->dropForeign( 'customerpenjualan_ifk');
-        });
+       
     }
 }
